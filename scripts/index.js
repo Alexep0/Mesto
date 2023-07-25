40e31a136c9a76453058b8a2453aed7e905f518b
Alexandr Popov <alexep130@yandex.ru>
2023-07-25 23:33:40 +0300

    add add-button

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,81 @@
-let content = document.querySelector('.content');
-let page = document.querySelector('.page');
+const content = document.querySelector('.content');
+const page = document.querySelector('.page');
+const popupEdit = page.querySelector('.popup_type_edit')
+const popupAdd = page.querySelector('.popup_type_add')
 
-let editButton = content.querySelector('.profile__edit-button');
-let closeEditButton = page.querySelector('.popup__button-close');
+const editButton = content.querySelector('.profile__edit-button');
+const closeEditButton = popupEdit.querySelector('.popup__button-close');
 
-let formElement = page.querySelector('.popup');
-let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#job');
-let profTitle = content.querySelector('.profile__title');
-let profDesc = content.querySelector('.profile__description');
+const addButton = content.querySelector('.profile__add-button');
+const closeAddButton = popupAdd.querySelector('.popup__button-close');
 
-function openForm() {
-    formElement.classList.add('popup_opened');
+
+const nameInput = popupEdit.querySelector('#name');
+const jobInput = popupEdit.querySelector('#job');
+const profTitle = content.querySelector('.profile__title');
+const profDesc = content.querySelector('.profile__description');
+
+function openEditForm() {
+    popupEdit.classList.add('popup_opened');
     nameInput.value = profTitle.innerText;
     jobInput.value = profDesc.innerText;
 }
-
-function closeForm() {
-    formElement.classList.remove('popup_opened');
+function closeEditForm() {
+    popupEdit.classList.remove('popup_opened');
 }
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    formElement.classList.remove('popup_opened');
+    popupEdit.classList.remove('popup_opened');
     profTitle.innerText = nameInput.value;
     profDesc.innerText = jobInput.value;
 
 }
 
-editButton.addEventListener('click', openForm);
-closeEditButton.addEventListener('click', closeForm);
-formElement.addEventListener('submit', handleFormSubmit); 
+editButton.addEventListener('click', openEditForm);
+closeEditButton.addEventListener('click', closeEditForm);
+popupEdit.addEventListener('submit', handleFormSubmit); 
+
+
+function openAddForm() {
+    popupAdd.classList.add('popup_opened');
+}
+
+function closeAddForm() {
+    popupAdd.classList.remove('popup_opened');
+}
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    popupAdd.classList.remove('popup_opened');
+
+}
+addButton.addEventListener('click', openAddForm);
+closeAddButton.addEventListener('click', closeAddForm);
+popupAdd.addEventListener('submit', handleFormSubmit); 
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+  console.log(initialCards[1].name);
